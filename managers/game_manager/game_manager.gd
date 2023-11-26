4bbe4af9631dcf39731dfdf18b93ea27dc0d9e6d
Jonathan Corona <jonathan.corona@battlerd.com>
2023-11-26 02:03:30 -0600

    Add time out condition to game over

--- managers/game_manager/game_manager.gd
+++ managers/game_manager/game_manager.gd
@@ -1,10 +1,30 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
+signal game_over
+## Time in seconds for gameplay duration from 1 minute to 10 minutes
+@export_range(60, 600) var time_out_value : float = 180
+@onready var time_out_manager: Timer = $"../TimeOutManager"
+
+var game_state : GAME_STATES = GAME_STATES.READY
+
+enum GAME_STATES {
+	READY,
+	PLAYING,
+	PAUSED,
+	GAME_OVER
+}
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	time_out_manager.init(time_out_value)
+	time_out_manager.time_is_eover.connect(on_time_is_eover)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func change_game_state(new_state: GAME_STATES) -> void:
+	game_state = new_state
+
+
+func on_time_is_eover() -> void:
+	change_game_state(GAME_STATES.GAME_OVER)
+	game_over.emit()
+
