be8430e5641c1ec091cd12a1fd660ec7922177df
Alex Ramirez <yaramirez68@gmail.com>
2023-11-26 02:21:22 -0500

    Auto stash before merge of "main" and "origin/main"

--- managers/game_manager/data/item_data.gd
+++ managers/game_manager/data/item_data.gd
@@ -13,22 +13,42 @@ var ItemDataSpawn: Dictionary = {
 	},
 	"ToyShark": {
 		"scene": preload("res://game_objects/item/items_main/item_shark.tscn"),
-		"position": []
+		"position": [
+			{ "x": 0.0, "y": 0.0, "z": 19.113 },
+			{ "x": 26.523, "y": 0.0, "z": 19.113 },
+			{ "x": 18.343, "y": 0.0, "z": 6.492 },
+		]
 	},
 	"ToyPigueon_03": {
 		"scene": preload("res://game_objects/item/items_main/item_pigueon_03.tscn"),
-		"position": []
+		"position": [
+			{ "x": 10.56, "y": 0.0, "z": -7.414 },
+			{ "x": 23.833, "y": 0.0, "z": -13.911 },
+			{ "x": 12.867, "y": 0.0, "z": -17.602 },
+		]
 	},
 	"ToyPigueon_02": {
 		"scene": preload("res://game_objects/item/items_main/item_pigueon_02.tscn"),
-		"position": []
+		"position": [
+			{ "x": 3.884, "y": 0.0, "z": -17.602 },
+			{ "x": -15.315, "y": 0.0, "z": -8.724 },
+			{ "x": -28.459, "y": 0.0, "z": 0.451 },
+		]
 	},
 	"ToyPigueon_01": {
 		"scene": preload("res://game_objects/item/items_main/item_pigueon_01.tscn"),
-		"position": []
+		"position": [
+			{ "x": -19.206, "y": 0.0, "z": -18.233 },
+			{ "x": -28.255, "y": 0.0, "z": -16.689 },
+			{ "x": -28.255, "y": 0.0, "z": 10.335 },
+		]
 	},
 	"ToyManta": {
 		"scene": preload("res://game_objects/item/items_main/item_manta.tscn"),
-		"position": []
+		"position": [
+			{ "x": -12.752, "y": 0.0, "z": 21.174 },
+			{ "x": 15.51, "y": 0.0, "z": 7.679 },
+			{ "x": -15.267, "y": 0.0, "z": 1.886 },
+		]
 	},
 }
