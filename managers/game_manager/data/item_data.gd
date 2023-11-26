2519083aff9f7cb227f9a97bceac84be928e7ffb
MaestroShifu <sdanielsarsantos@gmail.com>
2023-11-25 23:45:27 -0500

    se agrega el nodo game manager

---
+++ managers/game_manager/data/item_data.gd
@@ -0,0 +1,34 @@
+extends Resource
+
+class_name ItemSpawn
+
+var ItemDataSpawn: Dictionary = {
+	"ToySheep": {
+		"path": "res://game_objects/item/items_main/item_sheep.tscn",
+		"position": [
+			{ "x": 3.78, "y": 0.30, "z": -3.59 },
+			{ "x": 14.59, "y": 0.30, "z": 6.35 },
+			{ "x": -22.33, "y": 0.30, "z": 18.29 },
+		]
+	},
+	"ToyShark": {
+		"path": "res://game_objects/item/items_main/item_shark.tscn",
+		"position": []
+	},
+	"ToyPigueon_03": {
+		"path": "res://game_objects/item/items_main/item_pigueon_03.tscn",
+		"position": []
+	},
+	"ToyPigueon_02": {
+		"path": "res://game_objects/item/items_main/item_pigueon_02.tscn",
+		"position": []
+	},
+	"ToyPigueon_01": {
+		"path": "res://game_objects/item/items_main/item_pigueon_01.tscn",
+		"position": []
+	},
+	"ToyManta": {
+		"path": "res://game_objects/item/items_main/item_manta.tscn",
+		"position": []
+	},
+}
