a6844a828088022832811783d162fc7e05154fc2
MaestroShifu <sdanielsarsantos@gmail.com>
2023-11-26 18:22:52 -0500

    change value color

--- vfx/dog_signal/dog_signal.gd
+++ vfx/dog_signal/dog_signal.gd
@@ -5,7 +5,7 @@ class_name SignalVFX
 @onready var exclamation_mesh: GPUParticles3D = $Exclamation_mesh
 
 func boiling() -> void:
-	exclamation_mesh.process_material.color = Color(0, 0, 1, 1)
+	exclamation_mesh.process_material.color = Color(1, 0, 0, 1)
 
 func warm() -> void:
 	exclamation_mesh.process_material.color = Color(0, 1, 0, 1)
